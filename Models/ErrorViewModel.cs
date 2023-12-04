f330ac35b70a9f676da77ea33675b346bbd466f8
Oscar Enbacka <enbacka.oscar@gmail.com>
2023-12-04 11:27:35 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LiaW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
